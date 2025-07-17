20d3c59e269568ada57db2e63685a37df635a5f6
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-07-17 09:06:08 +0100

    DB connect

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
+import re
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://alexshanley:Facebook!2@localhost/petdb'
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,7 +30,6 @@ class Pet(db.Model):
 def index():
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
-
 
 @app.route('/pets')
 def list_pets():
@@ -46,9 +49,8 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-   
     with app.app_context():
-        db.create_all()
+        db.create_all()  
 
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
