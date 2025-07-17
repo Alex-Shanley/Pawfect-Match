8bcd08e684c531bf42369b3faf42bfeeb120f127
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-07-17 09:30:41 +0100

    url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pawfect_db_user:Bb8XbtoCWJV5mTKLw0FcE1tvKkUloeYR@dpg-d1saoeumcj7s73dgnqtg-a/pawfect_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
