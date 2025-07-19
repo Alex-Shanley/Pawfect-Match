7defedab26a514146dcfe25cc5fafc54eb38fee2
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-07-19 14:52:23 +0100

    js

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,13 @@ function hideSidebar() {
   const sidebar= document.querySelector('.sidebar')
   sidebar.style.display = 'none'
 }
+
+
+const items = document.querySelectorAll ('.faq-title');
+
+items.forEach (items => {
+    items.addEventListener('click', () => {
+        const parent = items.parentElement;
+        parent.classList.toggle('active');
+    });
+});
