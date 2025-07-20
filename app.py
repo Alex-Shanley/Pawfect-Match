22b161ec2d672ba3e785ce9f61e8a79c1efb5c2c
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-07-20 22:11:21 +0100

    bug fix

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = 'a8f3@9!gks92&x1z'
 
-
 database_url = os.environ.get('DATABASE_URL')
-
 if not database_url:
     database_url = 'sqlite:///test.db'
-
-
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 
@@ -31,30 +29,22 @@ class Pet(db.Model):
 
     def __repr__(self):
         return f'<Pet {self.name}>'
-    
-
-
 
 class Submission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    message = db.Column(db.Text, nullable=False)  # use Text for longer input
-    terms = db.Column(db.Boolean, nullable=False)  # checkbox => Boolean
+    message = db.Column(db.Text, nullable=False)
+    terms = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return f'<Submission {self.first_name} {self.surname}>'
 
-
-
 with app.app_context():
     db.create_all()
 
-
-
-
-@app.route('/index', methods =['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         first_name = request.form.get('first_name')
@@ -62,19 +52,9 @@ def index():
         email = request.form.get('email')
         message = request.form.get('message')
         terms = request.form.get('terms')
-
-        
-        flash ('Thank you for contacting us!!')
-
+        flash('Thank you for contacting us!!')
         return render_template('index.html', form_action=url_for('index'), first_name=first_name, surname=surname, email=email, message=message, terms=terms)
-        
-                            
     return render_template('index.html', form_action=url_for('index'))
-   
-
-
-
-
 
 @app.route('/pets')
 def list_pets():
@@ -85,9 +65,7 @@ def list_pets():
 def adopt():
     return render_template('adopt.html')
 
-
-
-@app.route('/about', methods =['GET', 'POST'])
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     if request.method == 'POST':
         first_name = request.form.get('first_name')
@@ -95,76 +73,39 @@ def about():
         email = request.form.get('email')
         message = request.form.get('message')
         terms = request.form.get('terms')
-
-        
-        flash ('Thank you for contacting us!!')
-
+        flash('Thank you for contacting us!!')
         return render_template('about.html', form_action=url_for('about'), first_name=first_name, surname=surname, email=email, message=message, terms=terms)
-        
-                               
-    
     return render_template('about.html', form_action=url_for('about'))
 
-
-
-
-
-
-
-
-
-@app.route('/contact', methods =['GET', 'POST'])
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
         first_name = request.form.get('first_name')
         surname = request.form.get('surname')
         email = request.form.get('email')
         message = request.form.get('message')
-        terms = request.form.get('terms')
-
-        
-        flash ('Thank you for contacting us!!')
+        terms = request.form.get('terms') == 'on'
+        submission = Submission(first_name=first_name, surname=surname, email=email, message=message, terms=terms)
+        db.session.add(submission)
+        db.session.commit()
+        flash('Thank you for contacting us!!')
         return redirect(url_for('contact'))
-    
     return render_template('contact.html')
 
-
-@app.route('/faq', methods =['GET', 'POST'])
+@app.route('/faq', methods=['GET', 'POST'])
 def faq():
     if request.method == 'POST':
         first_name = request.form.get('first_name')
         surname = request.form.get('surname')
         email = request.form.get('email')
         message = request.form.get('message')
-        terms = request.form.get('terms')
-
-
-        # Saving to database start
-
-        submission = Submission (
-            first_name=first_name,
-            surname=surname,
-            email=email,
-            message=message,
-            terms=terms
-        )
-
+        terms = request.form.get('terms') == 'on'
+        submission = Submission(first_name=first_name, surname=surname, email=email, message=message, terms=terms)
         db.session.add(submission)
         db.session.commit()
-
-
-        # Saving to database end
-
-        
-        flash ('Thank you for contacting us!!')
-
+        flash('Thank you for contacting us!!')
         return render_template('faq.html', form_action=url_for('faq'), first_name=first_name, surname=surname, email=email, message=message, terms=terms)
-        
-                               
-    
     return render_template('faq.html', form_action=url_for('faq'))
-
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
