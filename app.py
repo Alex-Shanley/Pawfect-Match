6aa0e8f27429cf60d0d585572c007937c26d384e
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-07-26 22:45:06 +0100

    debug

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def index():
         }
     ]
 
-    pets = pets[:4]
+
 
     if request.method == 'POST':
         first_name = request.form.get('first_name')
@@ -115,9 +115,9 @@ def index():
         message = request.form.get('message')
         terms = request.form.get('terms') == 'on'
         flash('Thank you for contacting us!!')
-        return render_template('index.html', form_action=url_for('index'), first_name=first_name, surname=surname, email=email, message=message, terms=terms, steps=steps,pets=pets)
+        return render_template('index.html', form_action=url_for('index'), first_name=first_name, surname=surname, email=email, message=message, terms=terms, steps=steps)
     
-    return render_template('index.html', steps=steps, form_action=url_for('index'),pets=pets)
+    return render_template('index.html', steps=steps, form_action=url_for('index'))
 
 
 # Pets listing page
